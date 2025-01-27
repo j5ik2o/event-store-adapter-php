c030848515013cb11292dbb107ac9c10311b0875
Junichi Kato <j5ik2o@gmail.com>
2025-01-28 02:37:21 +0900

    feat: add in-memory event store implementation

---
+++ src/Internal/EventStoreAsyncInMemory.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace J5ik2o\EventStoreAdapterPhp\Internal;
+
+use GuzzleHttp\Promise\FulfilledPromise;
+use GuzzleHttp\Promise\PromiseInterface;
+use J5ik2o\EventStoreAdapterPhp\Aggregate;
+use J5ik2o\EventStoreAdapterPhp\AggregateId;
+use J5ik2o\EventStoreAdapterPhp\Event;
+use J5ik2o\EventStoreAdapterPhp\EventSerializer;
+use J5ik2o\EventStoreAdapterPhp\EventStoreAsync;
+use J5ik2o\EventStoreAdapterPhp\KeyResolver;
+use J5ik2o\EventStoreAdapterPhp\SnapshotSerializer;
+use J5ik2o\EventStoreAdapterPhp\OptimisticLockException;
+
+class EventStoreAsyncInMemory implements EventStoreAsync {
+    private const INITIAL_VERSION = 1;
+    /** @var array<string, array<Event>> */
+    private array $events;
+
+    /** @var array<string, Aggregate> */
+    private array $snapshots;
+
+    public function __construct() {
+        $this->events = [];
+        $this->snapshots = [];
+    }
+
+    public function withKeepSnapshot(bool $keepSnapshot): EventStoreAsync {
+        return $this;
+    }
+
+    public function withDeleteTtl(int $deleteTtlInMillSec): EventStoreAsync {
+        return $this;
+    }
+
+    public function withKeepSnapshotCount(int $keepSnapshotCount): EventStoreAsync {
+        return $this;
+    }
+
+    public function withKeyResolver(KeyResolver $keyResolver): EventStoreAsync {
+        return $this;
+    }
+
+    public function withEventSerializer(EventSerializer $eventSerializer): EventStoreAsync {
+        return $this;
+    }
+
+    public function withSnapshotSerializer(SnapshotSerializer $snapshotSerializer): EventStoreAsync {
+        return $this;
+    }
+
+    public function persistEvent(Event $event, int $version): PromiseInterface {
+        $promise = new FulfilledPromise([$event, $version]);
+        $promise->then(function ($arg) {
+            /** @var Event $event */
+            /** @var int $version */
+            [$event, $version] = $arg;
+            if ($event->isCreated()) {
+                throw new \RuntimeException('event is created');
+            }
+
+            $aggregateId = $event->getAggregateId()->asString();
+
+            if ($this->snapshots[$aggregateId]->getVersion() !== $version) {
+                throw new OptimisticLockException(
+                    'Transaction write was canceled due to conditional check failure'
+                );
+            }
+
+            $newVersion = $this->snapshots[$aggregateId]->getVersion() + 1;
+            $this->events[$aggregateId][] = $event;
+
+            $snapshot = $this->snapshots[$aggregateId];
+            $snapshot = $snapshot->withVersion($newVersion);
+            $this->snapshots[$aggregateId] = $snapshot;
+        });
+        return $promise;
+    }
+
+    public function persistEventAndSnapshot(Event $event, Aggregate $aggregate): PromiseInterface {
+        $promise = new FulfilledPromise([$event, $aggregate]);
+        $promise->then(function ($arg) {
+            /** @var Event $event */
+            /** @var Aggregate $aggregate */
+            [$event, $aggregate] = $arg;
+            $aggregateId = $event->getAggregateId()->asString();
+            $newVersion = self::INITIAL_VERSION;
+
+            if (!$event->isCreated()) {
+                $version = $this->snapshots[$aggregateId]->getVersion();
+                if ($version !== $aggregate->getVersion()) {
+                    throw new OptimisticLockException(
+                        'Transaction write was canceled due to conditional check failure'
+                    );
+                }
+                $newVersion = $this->snapshots[$aggregateId]->getVersion() + 1;
+            }
+
+            $this->events[$aggregateId][] = $event;
+            $this->snapshots[$aggregateId] = $aggregate->withVersion($newVersion);
+            return null;
+        });
+        return $promise;
+    }
+
+    public function getLatestSnapshotById(AggregateId $aggregateId): PromiseInterface {
+        $promise = new FulfilledPromise($aggregateId);
+        $promise->then(function (AggregateId $aggregateId) {
+            $id = $aggregateId->asString();
+            if (isset($this->snapshots[$id])) {
+                return $this->snapshots[$id];
+            }
+            return null;
+        });
+        return $promise;
+    }
+
+    public function getEventsByIdSinceSequenceNumber(AggregateId $aggregateId, int $sequenceNumber): PromiseInterface {
+        $promise = new FulfilledPromise([$aggregateId, $sequenceNumber]);
+        $promise->then(function ($arg) {
+            /** @var AggregateId $aggregateId */
+            /** @var int $sequenceNumber */
+            [$aggregateId, $sequenceNumber] = $arg;
+            $result = [];
+            $id = $aggregateId->asString();
+
+            if (!isset($this->events[$id])) {
+                return $result;
+            }
+
+            foreach ($this->events[$id] as $event) {
+                if ($event->getSequenceNumber() >= $sequenceNumber) {
+                    $result[] = $event;
+                }
+            }
+            return $result;
+        });
+        return $promise;
+    }
+}
---
+++ src/Internal/EventStoreInMemory.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace J5ik2o\EventStoreAdapterPhp\Internal;
+
+use J5ik2o\EventStoreAdapterPhp\Aggregate;
+use J5ik2o\EventStoreAdapterPhp\AggregateId;
+use J5ik2o\EventStoreAdapterPhp\Event;
+use J5ik2o\EventStoreAdapterPhp\EventSerializer;
+use J5ik2o\EventStoreAdapterPhp\EventStore;
+use J5ik2o\EventStoreAdapterPhp\KeyResolver;
+use J5ik2o\EventStoreAdapterPhp\OptimisticLockException;
+use J5ik2o\EventStoreAdapterPhp\SnapshotSerializer;
+
+class EventStoreInMemory implements EventStore {
+    private const INITIAL_VERSION = 1;
+
+    /** @var array<string, array<Event>> */
+    private array $events;
+
+    /** @var array<string, Aggregate> */
+    private array $snapshots;
+
+    public function __construct() {
+        $this->events = [];
+        $this->snapshots = [];
+    }
+
+    public function withKeepSnapshot(bool $keepSnapshot): EventStore {
+        return $this;
+    }
+
+    public function withDeleteTtl(int $deleteTtlInMillSec): EventStore {
+        return $this;
+    }
+
+    public function withKeepSnapshotCount(int $keepSnapshotCount): EventStore {
+        return $this;
+    }
+
+    public function withKeyResolver(KeyResolver $keyResolver): EventStore {
+        return $this;
+    }
+
+    public function withEventSerializer(EventSerializer $eventSerializer): EventStore {
+        return $this;
+    }
+
+    public function withSnapshotSerializer(SnapshotSerializer $snapshotSerializer): EventStore {
+        return $this;
+    }
+
+    public function persistEvent(Event $event, int $version): void {
+        if ($event->isCreated()) {
+            throw new \RuntimeException('event is created');
+        }
+
+        $aggregateId = $event->getAggregateId()->asString();
+
+        if ($this->snapshots[$aggregateId]->getVersion() !== $version) {
+            throw new OptimisticLockException(
+                'Transaction write was canceled due to conditional check failure'
+            );
+        }
+
+        $newVersion = $this->snapshots[$aggregateId]->getVersion() + 1;
+        $this->events[$aggregateId][] = $event;
+
+        $snapshot = $this->snapshots[$aggregateId];
+        $snapshot = $snapshot->withVersion($newVersion);
+        $this->snapshots[$aggregateId] = $snapshot;
+    }
+
+    public function persistEventAndSnapshot(Event $event, Aggregate $aggregate): void {
+        $aggregateId = $event->getAggregateId()->asString();
+        $newVersion = self::INITIAL_VERSION;
+
+        if (!$event->isCreated()) {
+            $version = $this->snapshots[$aggregateId]->getVersion();
+            if ($version !== $aggregate->getVersion()) {
+                throw new OptimisticLockException(
+                    'Transaction write was canceled due to conditional check failure'
+                );
+            }
+            $newVersion = $this->snapshots[$aggregateId]->getVersion() + 1;
+        }
+
+        $this->events[$aggregateId][] = $event;
+        $this->snapshots[$aggregateId] = $aggregate->withVersion($newVersion);
+    }
+
+    public function getLatestSnapshotById(AggregateId $aggregateId): ?Aggregate {
+        $id = $aggregateId->asString();
+        if (isset($this->snapshots[$id])) {
+            return $this->snapshots[$id];
+        }
+        return null;
+    }
+
+    public function getEventsByIdSinceSequenceNumber(AggregateId $aggregateId, int $sequenceNumber): array {
+        $result = [];
+        $id = $aggregateId->asString();
+
+        if (!isset($this->events[$id])) {
+            return $result;
+        }
+
+        foreach ($this->events[$id] as $event) {
+            if ($event->getSequenceNumber() >= $sequenceNumber) {
+                $result[] = $event;
+            }
+        }
+        return $result;
+    }
+}
